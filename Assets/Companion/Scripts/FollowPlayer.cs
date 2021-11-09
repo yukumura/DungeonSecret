d06707b558344e982b1ba31b7b7946619d04bf0a
yukumura <yukumuradota@gmail.com>
2021-11-09 21:11:23 +0100

    add companion

---
+++ Assets/Companion/Scripts/FollowPlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] 
+    Transform movePositionTransform;
+
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.destination = movePositionTransform.position;
+    }
+}
